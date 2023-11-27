bb7645c1ad04c21a7e26c2f7bed340707e7541fe
JeremieCius <jeremiecius@gmail.com>
2023-11-27 10:39:45 -0700

    init build

---
+++ MadForInputsREVAMPED/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MadForInputsREVAMPED.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MadForInputsREVAMPED.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult DisplayMadlib()
+        {
+            Madlib madlib = new Madlib(Request.Form["NameBox"], Request.Form["LocationBox"],
+                Request.Form["VerbBox"], Request.Form["AdverbBox"],
+                Request.Form["AdjectiveBox"]);
+            return View(madlib);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MadForInputsREVAMPED/Models/Madlib.cs
@@ -0,0 +1,25 @@
+﻿namespace MadForInputsREVAMPED.Models
+{
+    public class Madlib
+    {
+        public string Name { get; set; } = "Seth";
+        public string Location { get; set; } = "Place";
+        public string Verb { get; set; } = "Vomit";
+        public string Adverb { get; set; } = "freely";
+        public string Adjective { get; set; } = "deadly";
+
+        public Madlib()
+        {
+
+        }
+
+        public Madlib(string name, string location, string verb, string adverb, string adjective)
+        {
+            this.Name = name;
+            this.Location = location;
+            this.Verb = verb;
+            this.Adverb = adverb;
+            this.Adjective = adjective;
+        }
+    }
+}
