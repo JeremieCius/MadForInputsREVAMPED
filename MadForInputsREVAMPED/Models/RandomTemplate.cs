c104f734f79170a84363cd7642dc9cf37a8ffe6a
JeremieCius <jeremiecius@gmail.com>
2023-12-03 20:12:18 -0800

    RandomTemplate

---
+++ MadForInputsREVAMPED/Models/RandomTemplate.cs
@@ -0,0 +1,183 @@
+﻿namespace MadForInputsREVAMPED.Models
+{
+    public class RandomTemplate
+    {
+        public string NounOne { get; set; }
+
+        public string NounTwo { get; set; }
+
+        public string NounThree { get; set; }
+
+        public string NounFour { get; set; } 
+
+        public string NounFive { get; set; }
+
+
+        public string AdjectiveOne { get; set; }
+
+        public string AdjectiveTwo { get; set; }
+
+        public string AdjectiveThree { get; set; }
+
+        public string AdjectiveFour { get; set; }
+
+        public string AdjectiveFive { get; set; }
+
+        public string VerbOne { get; set; } 
+
+        public string VerbTwo { get; set; }
+
+        public string VerbThree { get; set; }
+
+        public string VerbFour { get; set; }
+
+        public string VerbFive { get; set; }
+
+        public string VerbSix { get; set; }
+
+        public string VerbSeven { get; set; }
+
+        public string AdverbOne { get; set; } 
+
+        public string AdverbTwo { get; set; }
+
+        public string AdverbThree { get; set; }
+
+        public string AdverbFour { get; set; }
+
+
+        public RandomTemplate()
+        {
+
+        }
+
+        public RandomTemplate(string nounOne, string nounTwo, string nounThree, string nounFour, string nounFive, string adjectiveOne, string adjectiveTwo, string adjectiveThree, string adjectiveFour, string adjectiveFive,string verbOne, string verbTwo, string verbThree, string verbFour, string verbFive, string verbSix, string verbSeven, string adverbOne, string adverbTwo, string adverbThree, string adverbFour)
+        {
+            NounOne = nounOne;
+            NounTwo = nounTwo;
+            NounThree = nounThree;
+            NounFour = nounFour;
+            NounFive = nounFive;
+            AdjectiveOne = adjectiveOne;
+            AdjectiveTwo = adjectiveTwo;
+            AdjectiveThree = adjectiveThree;
+            AdjectiveFour = adjectiveFour;
+            AdjectiveFive = adjectiveFive;
+            VerbOne = verbOne;
+            VerbTwo = verbTwo;
+            VerbThree = verbThree;
+            VerbFour = verbFour;
+            VerbFive = verbFive;
+            VerbSix = verbSix;
+            VerbSeven = verbSeven;
+            AdverbOne = adverbOne;
+            AdverbTwo = adverbTwo;
+            AdverbThree = adverbThree;
+            AdverbFour = adverbFour;
+        }
+
+
+        public override string ToString()
+        {
+            Random prompt = new Random();
+            string story = "";
+
+            switch (prompt.Next(3, 1))
+            {
+                case 1:
+                    story = $@"
+                    Breaking News Report
+
+Good evening, ladies and gentlemen. This is {NounOne} Johnson, reporting live from the heart of {NounThree}ville. We bring you an unprecedented event that has left the entire community both bewildered and in stitches.
+
+In a bizarre turn of events, a group of {AdjectiveOne} {NounFour} enthusiasts, led by the charismatic {AdjectiveTwo} {NounFive}, has taken the town by storm. Witnesses report that the group, armed with {AdjectiveThree} {VerbOne}, descended upon the central square, where they unveiled their master plan.
+
+With {AdverbOne} precision, the group initiated a series of {VerbTwo} demonstrations, showcasing their prowess in the world of {NounTwo}-related activities. Passersby were treated to a spectacle of {VerbThree} and {VerbFour} that defied both logic and expectation.
+
+As the crowd gathered, the {AdjectiveFour} mayor of {NounThree}ville, who happened to be enjoying an {AdjectiveFive} sandwich at a nearby cafe, expressed both confusion and amusement at the unfolding events. ""I never thought I'd see the day when our town became the epicenter of {NounOne}-related shenanigans,"" said the mayor.
+
+In an attempt to make sense of the situation, our team interviewed the town's {AdverbTwo} renowned {NounOne} expert, Professor {AdverbThree} {NounTwo}. The professor, known for his {AdjectiveTwo} knowledge in {NounFour} dynamics, admitted that even he was struggling to comprehend the magnitude of the {AdjectiveFive} spectacle.
+
+Eyewitnesses also reported a surprising twist as the group introduced a series of {AdverbFour} {VerbFive} challenges. Local residents, lured by the promise of {AdjectiveFour} prizes, eagerly participated in the hilarious contests that involved everything from {VerbSix} with {NounFour} to {VerbSeven} in {AdverbThree} slow motion.
+
+As the sun sets on this peculiar day in {NounThree}ville, the townsfolk are left with a mixture of laughter and confusion. The {AdjectiveOne} antics of the {NounFour} enthusiasts have turned an ordinary day into an extraordinary event that will undoubtedly be etched in the annals of {NounThree}ville's history.
+
+Stay tuned for further updates on this developing story. This is {NounOne} Johnson, signing off from {NounThree}ville, where the unexpected has become the new norm.                 
+";
+                    break;
+
+                case 2:
+                    story = $@"
+                    
+Whimsical {NounFour} Surprise Recipe
+
+Ingredients:
+
+2 cups {AdjectiveOne} {NounFour} chunks
+1 cup {AdjectiveTwo} {NounFour} slices
+1/2 cup {AdjectiveThree} {NounFour} shreds
+1/4 cup {AdjectiveFour} {NounFour} crumbs
+3 tablespoons {VerbOne} {NounFour} essence
+1 tablespoon {VerbTwo} {NounFour} zest
+A pinch of {AdverbOne} {NounFour} magic
+{AdverbTwo} sprinkle of {AdjectiveFive} {NounFour} dust
+Instructions:
+
+Begin by preheating your {NounThree}ville oven to 350°F (175°C). Make sure it's warmed up to embrace the whimsy.
+
+In a {NounFour}-sized mixing bowl, combine the {AdjectiveOne} {NounFour} chunks, {AdjectiveTwo} {NounFour} slices, and {AdjectiveThree} {NounFour} shreds. Gently toss the {NounFour} pieces together, creating a harmonious medley of flavors.
+
+Sprinkle the {VerbOne} {NounFour} essence over the mixture. This will infuse the {NounFour} with a burst of delightful {VerbOne} goodness.
+
+Add the {VerbTwo} {NounFour} zest to the concoction. This step ensures a zesty kick that will awaken your taste buds to the playful nature of this dish.
+
+Carefully incorporate the {AdjectiveFour} {NounFour} crumbs, creating a delightful crunch that contrasts beautifully with the tender {NounFour} pieces.
+
+Now, for the secret ingredient – a pinch of {AdverbOne} {NounFour} magic. Channel your inner {NounOne} expert and sprinkle the magic over the mixture. This step adds a touch of enchantment to your culinary creation.
+
+Gently toss everything together until the {NounFour} is coated in the magical blend. Allow the flavors to mingle and dance in perfect harmony.
+
+Transfer the {NounFour} mixture to a {AdjectiveFive} baking dish, spreading it evenly. Sprinkle a {AdverbTwo} dusting of {AdjectiveFive} {NounFour} dust on top for that extra dash of whimsy.
+
+Bake in the preheated {NounThree}ville oven for 25–30 minutes or until the {NounFour} is golden brown and exuding an irresistible aroma.
+
+Once baked to perfection, remove the {NounFour} surprise from the oven and let it cool for a few minutes. Then, serve your whimsical creation with a side of laughter and enjoy the delightful taste of {NounFour} magic!
+
+Note: This recipe is guaranteed to bring joy and laughter to your {NounThree}ville dining experience. Adjust ingredient quantities based on your desired level of whimsy. Enjoy!
+";
+                    break;
+
+                case 3:
+                    story = $@"
+                    Proposal for the Federal Legalization of {NounOne}
+
+Executive Summary:
+In recent years, there has been a growing call for the federal legalization of {NounOne}, driven by the undeniable benefits and the potential for creating a more vibrant and whimsical society. This proposal aims to explore the various aspects that support the legalization of {NounOne} on the federal level, highlighting its positive impact on the economy, health, and overall well-being of the nation.
+
+Introduction:
+{NounOne}, often misunderstood and unjustly maligned, holds immense potential to revolutionize our society. This proposal seeks to shed light on the myriad reasons why {NounOne} should be embraced, celebrated, and, most importantly, legalized at the federal level.
+
+Economic Impact:
+Legalizing {NounOne} has the potential to stimulate economic growth in ways that are both unexpected and delightful. The {AdjectiveOne} industry surrounding {NounOne} cultivation and distribution could create a plethora of jobs, from {VerbOne} experts to {AdjectiveTwo} {NounOne} chefs. Additionally, the creation of {AdjectiveThree} {NounOne} dispensaries could become vibrant hubs of economic activity, drawing visitors from across the nation.
+
+Health and Wellness:
+Contrary to popular belief, {NounOne} has been proven to have a positive impact on health and wellness. {VerbTwo} with {NounOne} has been linked to increased {AdjectiveFour} levels and a noticeable reduction in stress. Incorporating {NounOne} into daily routines has the potential to foster a healthier and more relaxed society.
+
+Environmental Benefits:
+{NounOne} cultivation has shown promise as a {VerbThree} alternative to traditional crops, requiring less {NounFour} and producing {AdjectiveFour} yields. Embracing {NounOne} on a federal level could pave the way for sustainable agricultural practices, contributing to a healthier planet for future generations.
+
+Social Harmony:
+The legalization of {NounOne} can foster a sense of social harmony and unity. Picture a world where {AdjectiveFive} {NounOne} festivals bring people together, breaking down barriers and promoting a shared sense of joy and merriment. Communities could bond over their shared appreciation for the versatile applications of {NounOne}.
+
+Regulation and Control:
+By legalizing {NounOne} at the federal level, we open the door to effective regulation and control. Establishing {AdverbOne} guidelines for {NounOne} use will ensure that its benefits are harnessed responsibly, preventing misuse and promoting a safe and enjoyable experience for all.
+
+Conclusion:
+In conclusion, the federal legalization of {NounOne} is not just a legislative matter; it's an opportunity to usher in a new era of joy, innovation, and well-being. By embracing the whimsical potential of {NounOne}, we can create a society that thrives on laughter, unity, and the shared experience of a {NounOne}-infused world. It's time to elevate our nation to new heights of delight, and the legalization of {NounOne} is the key to unlocking this brighter, more cheerful future.
+";
+                    break;
+            }
+            return story;
+        }
+    }
+}
