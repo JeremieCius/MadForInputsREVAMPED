a66fbe27f3e9b3c9f3edf9f6608951d5149f402e
JeremieCius <jeremiecius@gmail.com>
2023-12-05 21:14:14 -0700

    Context and User

--- MadForInputsREVAMPED/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MadForInputsREVAMPED/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using System;
 
 namespace MadForInputsREVAMPED.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(MadlibContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
         protected override void BuildModel(ModelBuilder modelBuilder)
