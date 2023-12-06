fc17213f7e0224933960bf5a9c7e0d75efd76b97
JeremieCius <jeremiecius@gmail.com>
2023-12-06 12:55:44 -0700

    Packages are weird

--- MadForInputsREVAMPED/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ MadForInputsREVAMPED/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace MadForInputsREVAMPED.Data.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(MadlibContext))]
     [Migration("00000000000000_CreateIdentitySchema")]
     partial class CreateIdentitySchema
     {
