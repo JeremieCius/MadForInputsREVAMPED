8acbb6bdd6270a12919d9b629a9901395bdcd841
JeremieCius <jeremiecius@gmail.com>
2023-12-07 04:25:30 -0700

    DbInit plus cleanup
    Filter still needs work as it does not work at all. Other than that plus how UGLY the site looks, it's good enough for me to go to bed

---
+++ MadForInputsREVAMPED/Data/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MadForInputsREVAMPED.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(MadlibContext context)
+        {
+            context.Database.EnsureCreated();
+
+            SetupUsers(context);
+        }
+
+        public static void SetupUsers(MadlibContext context)
+        {
+            if (context.Madlibs.Any())
+            {
+                return;
+            }
+        }
+    }
+}
